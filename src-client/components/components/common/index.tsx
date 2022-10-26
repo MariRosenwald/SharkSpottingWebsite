24949b91e3f2940fedc5fc349779454ed34e9868
MariRosenwald <45806411+MariRosenwald@users.noreply.github.com>
2022-10-26 12:13:58 -0700

    Delete src-client/components directory

--- src-client/components/components/common/index.tsx
+++
@@ -1,7 +0,0 @@
-import Header from './header'; 
-import Navbar from './navbar';
-
-export{
-    Header, 
-    Navbar
-}
--- src-client/components/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-
-import { Page } from './components/Page';
-
-ReactDOM.render(
-    <Page/>,
-    document.getElementById('BaseContainer')
-);

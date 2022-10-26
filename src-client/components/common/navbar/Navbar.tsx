7104eeeaa2a65fec5bb93edec51afb76b5424387
MariRosenwald <45806411+MariRosenwald@users.noreply.github.com>
2022-10-26 12:15:04 -0700

    Add files via upload

---
+++ src-client/components/common/navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+    return(
+        <section>
+            Navbar
+        </section>
+    );
+}
+
+export default Navbar; 

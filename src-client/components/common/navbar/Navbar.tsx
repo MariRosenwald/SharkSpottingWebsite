02e5bff5f23d510f102ceaf112d498cfe5b34680
MariRosenwald <45806411+MariRosenwald@users.noreply.github.com>
2022-10-26 12:25:44 -0700

    links in this one

--- src-client/components/common/navbar/Navbar.tsx
+++ src-client/components/common/navbar/Navbar.tsx
@@ -4,7 +4,18 @@ import './Navbar.css';
 function Navbar() {
     return(
         <section>
-            Navbar
+            <section className='About-sec'>
+                <a href="/" className="About">about</a>
+            </section>
+            <section className='Teams-sec'>
+                <a href="/" className="Teams">teams</a>
+            </section>
+            <section className='Memebers-sec'>
+                <a href="/" className="Members">members</a>
+            </section>
+            <section className='News-sec'>
+                <a href="/" className="News">news</a>
+            </section>
         </section>
     );
 }

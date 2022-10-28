925d87a45353904ffa3d6dcfe9351a83dc9ef57e
Jonathan Serdinsky <serdinskyj@gmail.com>
2022-10-28 12:42:55 -0700

    updated Navbar

--- src-client/components/common/navbar/Navbar.tsx
+++ src-client/components/common/navbar/Navbar.tsx
@@ -1,21 +1,19 @@
 import * as React from "react";
 import "./Navbar.css";
 import CSS from "csstype";
-import { isWhiteSpaceLike } from "typescript";
 
 const flex: CSS.Properties = {
   display: "flex",
   marginTop: "2rem",
   position: "absolute",
-  verticalAlign: "middle",
-  left: "50%",
+  left: "40%",
   transform: "translate(-50%, -50%)",
   color: "white",
   padding: "2rem",
 };
 const headerLinks: CSS.Properties = {
   color: "white",
-  margin: "20%",
+  margin: "10%",
 };
 function Navbar() {
   return (
@@ -24,18 +22,12 @@ function Navbar() {
         <a style={headerLinks} href="/" className="About">
           about
         </a>
-      </section>
-      <section className="Teams-sec">
         <a style={headerLinks} href="/" className="Teams">
           teams
         </a>
-      </section>
-      <section className="Memebers-sec">
         <a style={headerLinks} href="/" className="Members">
           members
         </a>
-      </section>
-      <section className="News-sec">
         <a style={headerLinks} href="/" className="News">
           news
         </a>

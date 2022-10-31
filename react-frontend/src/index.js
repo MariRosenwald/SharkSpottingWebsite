a24ef67d8cdc85f105fc92acf1be2a62d779814a
Peter Marsh <peter_marsh@hotmail.com>
2022-10-30 20:29:16 -0700

    added readme, deleted unused files

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOMClient from 'react-dom/client';
 import { Page } from './components/Page';
-import './index.css';
 
 const container = document.getElementById('root');
 

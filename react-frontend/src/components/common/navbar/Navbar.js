c1dc3d0d8277740b64ba0f51adfd2a17345c91f2
MariRosenwald <mwr.inbox@gmail.com>
2022-11-04 09:01:57 -0700

    Nav bar css fixed, still working on header

--- react-frontend/src/components/common/navbar/Navbar.js
+++ react-frontend/src/components/common/navbar/Navbar.js
@@ -1,37 +1,31 @@
 import * as React from 'react';
 import './Navbar.css';
 
-const flex = {
-  display: 'flex',
-  marginTop: '2rem',
-  position: 'absolute',
-  left: '45%',
-  transform: 'translate(-50%, -50%)',
-  color: 'white',
-  padding: '2rem'
-};
-const headerLinks = {
-  color: 'white',
-  paddingLeft: '10%',
-  paddingRight: '10%'
-};
+
+
 export function Navbar() {
   return (
-    <section style={flex}>
-      <section className="About-sec">
-        <a style={headerLinks} href="/" className="About">
-          about
-        </a>
-        <a style={headerLinks} href="/teams" className="Teams">
-          teams
-        </a>
-        <a style={headerLinks} href="/members" className="Members">
-          members
-        </a>
-        <a style={headerLinks} href="/news" className="News">
-          news
-        </a>
-      </section>
-    </section>
+    <div className="navbar">
+      <a href="/">About</a>
+      <a href="/teams">Teams</a>
+      <a href="/members">Members</a>
+      <a href="/news">News</a>
+    </div>
+    // <section style={flex}>
+    //   <section className="About-sec">
+    //     <a style={headerLinks} href="/" className="About">
+    //       about
+    //     </a>
+    //     <a style={headerLinks} href="/teams" className="Teams">
+    //       teams
+    //     </a>
+    //     <a style={headerLinks} href="/members" className="Members">
+    //       members
+    //     </a>
+    //     <a style={headerLinks} href="/news" className="News">
+    //       news
+    //     </a>
+    //   </section>
+    // </section>
   );
 }

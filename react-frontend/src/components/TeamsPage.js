5a4b0e42d1c5650d6d4d05797260bb1abeb2e90e
Jonathan Serdinsky <serdinskyj@gmail.com>
2022-11-06 21:00:59 -0800

    Simple backend + table implementation

--- react-frontend/src/components/TeamsPage.js
+++ react-frontend/src/components/TeamsPage.js
@@ -2,12 +2,13 @@ import * as React from 'react';
 //import axios from 'axios';
 import { Header } from './common/header/Header';
 import './Pages.css';
-
+import { PostLogin } from './PostLoginPage';
 export function TeamsPage() {
   return (
     <div>
       <Header />
       <h1 className="heading">Teams</h1>
+      <PostLogin />
     </div>
   );
 }

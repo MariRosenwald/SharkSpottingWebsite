fabfcdf1e1eedb55a7479ee438e1d0a9d3b51148
Peter Marsh <peter_marsh@hotmail.com>
2022-11-07 11:45:39 -0800

    backend authentication

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -1,135 +1,93 @@
-const cors = require("cors");
-const express = require("express");
+const cors = require('cors');
+const express = require('express');
 const app = express();
 const port = 5050;
 
-const users = {
-  users_list: [
-    {
-      id: "xyz789",
-      name: "Charlie",
-      job: "Janitor",
-    },
-    {
-      id: "abc123",
-      name: "Mac",
-      job: "Bouncer",
-    },
-    {
-      id: "ppp222",
-      name: "Mac",
-      job: "Professor",
-    },
-    {
-      id: "yat999",
-      name: "Dee",
-      job: "Aspring actress",
-    },
-    {
-      id: "zap555",
-      name: "Dennis",
-      job: "Bartender",
-    },
-  ],
-};
-
-const data = {
-  data_list: [
-    {
-      title: "shark pics",
-      location: "https://google.com",
-      description: "test",
-    },
-    {
-      title: "boat pics",
-      location: "https://www.yahoo.com/?guccounter=1",
-      description: "backend validation",
-    },
-  ],
-};
+const users = { 
+    users_list :
+    [
+       { 
+          email : 'pkmarsh@calpoly.edu',
+          password : 'dog'
+       },
+       {
+          email : 'mari@mari.com',
+          password : 'cat'
+       }
+    ]
+ }
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
 
-app.get("/user", (req, res) => {
-  res.send(data);
-});
-app.get("/users", (req, res) => {
-  const name = req.query.name;
-  const job = req.query.job;
-  filteredUsers = users["users_list"];
-  if (name != undefined) {
-    filteredUsers = filteredUsers.filter((user) => user["name"] === name);
-  }
-  if (job != undefined) {
-    filteredUsers = filteredUsers.filter((user) => user["job"] === job);
-  }
-  result = { users_list: filteredUsers };
-  res.send(result);
-});
-
-app.get("/users/:id", (req, res) => {
-  const id = req.params["id"]; //or req.params.id
-  let result = findUserById(id);
-  if (result === undefined || result.length == 0)
-    res.status(404).send("Resource not found.");
-  else {
-    result = { users_list: result };
-    res.send(result);
-  }
-});
-
-function findUserById(id) {
-  return users["users_list"].find((user) => user["id"] === id); // or line below
-}
-
-app.post("/users", (req, res) => {
-  const userToAdd = req.body;
-  userJson = addUser(userToAdd);
-  res.status(201).send(userJson).end();
+app.get('/auth', (req, res) => {
+    const email = req.query.email
+    const pwd = req.query.pwd
+    if (email === undefined) {
+        res.status(500).send("No email specified").end();
+    }
+    else if (pwd === undefined) {
+        res.status(500).send("No password specified").end();
+    }
+    
+    else {
+        authenticated = false
+        filteredUsers = users['users_list'].filter((user) =>
+            user['email'] === email
+        );
+        if (filteredUsers.length < 1) {
+            res.status(404).send(`No user '${email}' found`).end();
+        }
+        user = filteredUsers[0]
+        user_pwd = user['password']
+        if (user_pwd == pwd) {
+            authenticated = true
+        }
+        res.send(authenticated);
+    }   
 });
 
+/*
 function generateRandomUniqueID() {
-  allowedCharacters =
-    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  id = "";
-  id_length = 6;
-  do {
-    for (i = 0; i < id_length; i++) {
-      index = Math.floor(Math.random() * allowedCharacters.length);
-      id += allowedCharacters[index];
-    }
-  } while (users.users_list.filter((user) => user.id == id).length > 0);
-  return id;
+    allowedCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    id = "";
+    id_length = 6;
+    do {
+        for (i = 0; i < id_length; i++) {
+            index = Math.floor(Math.random()*allowedCharacters.length);
+            id += allowedCharacters[index];
+        }
+    } while (users.users_list.filter(user => user.id == id).length > 0)
+    return id;
 }
 
-function addUser(user) {
-  user["id"] = generateRandomUniqueID();
-  users["users_list"].push(user);
-  return user;
+function addUser(user){
+    user['id'] = generateRandomUniqueID();
+    users['users_list'].push(user);
+    return user;
 }
 
-app.delete("/users", (req, res) => {
-  const id = req.query.id;
-  if (id === undefined) {
-    res.status(500).send("No ID specified").end();
-  } else {
-    console.log(users.users_list);
-    index = users.users_list.findIndex((user) => user.id == id);
-    if (index == -1) {
-      res.status(404).statusMessage(`ID "${id}" does not exist`).end();
+app.delete('/users', (req, res) => {
+    const id = req.query.id;
+    if (id === undefined) {
+        res.status(500).send("No ID specified").end();
+    } else {
+        console.log(users.users_list);
+        index = users.users_list.findIndex(user => user.id == id);
+        if (index == -1) {
+            res.status(404).statusMessage(`ID "${id}" does not exist`).end();
+        }
+        console.log(index);
+        users.users_list.splice(index, 1);
+        console.log(users.users_list);
     }
-    console.log(index);
-    users.users_list.splice(index, 1);
-    console.log(users.users_list);
-  }
-  res.status(204).end();
+    res.status(204).end();
 });
-
+*/
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+    console.log(`Example app listening at http://localhost:${port}`);
+});      

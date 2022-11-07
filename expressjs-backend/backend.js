7a51f2cce12cb29cc1fe807d5235acaf88ad55e2
Peter Marsh <peter_marsh@hotmail.com>
2022-11-07 13:45:19 -0800

    update port for azure

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -88,6 +88,7 @@ app.delete('/users', (req, res) => {
     res.status(204).end();
 });
 */
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});      
+
+app.listen(process.env.PORT || port, () => {
+    console.log("REST API is listening.");
+});

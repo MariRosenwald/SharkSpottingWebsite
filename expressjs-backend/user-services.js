b6d84bfb4df7c2eae87a325b4637c33408b5a748
EvanJSpringer <81826544+EvanJSpringer@users.noreply.github.com>
2022-11-14 13:20:34 -0800

    Cloud database

--- expressjs-backend/user-services.js
+++ expressjs-backend/user-services.js
@@ -1,14 +1,29 @@
 const mongoose = require("mongoose");
 const User = require("./user");
+const dotenv = require("dotenv");
 mongoose.set("debug", true);
 
 let connected = false;
 
+dotenv.config();
+
 mongoose
-  .connect("mongodb://localhost:27017/users", {
-    useNewUrlParser: true,
+.connect(
+  "mongodb+srv://" +
+    process.env.MONGO_USER +
+    ":" +
+    process.env.MONGO_PWD +
+    "@" +
+    process.env.MONGO_CLUSTER +
+    "/" +
+    process.env.MONGO_DB +
+    "?retryWrites=true&w=majority",
+  // "mongodb://localhost:27017/users",
+  {
+    useNewUrlParser: true, //useFindAndModify: false,
     useUnifiedTopology: true,
-  })
+  }
+)
   .then(() => {
     connected = true
   })

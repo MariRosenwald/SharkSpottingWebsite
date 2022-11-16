1c976c704151a2803b231ef2b42c683abad9fab5
MariRosenwald <45806411+MariRosenwald@users.noreply.github.com>
2022-11-16 12:47:58 -0800

    Updated frontend

--- react-frontend/src/components/NewsPage.js
+++ react-frontend/src/components/NewsPage.js
@@ -6,8 +6,20 @@ import './Pages.css';
 export function NewsPage() {
   return (
     <div>
-      <Header />
-      <h1 className="heading">News</h1>
+      <div>
+        <Header />
+        <h1 className="heading">News</h1>
+      </div>
+      <div className="column">
+      <div className="card">
+        {/* <img src={NP} alt ="MN" width="100" /> */}
+        <div className="container">
+          <h2>Mustang News: Students improve shark-spotting technology</h2>
+          <a href="https://mustangnews.net/students-improve-shark-spotting-technology/">Article</a>
+          {/* <p><button className="button">Contact</button></p> */}
+        </div>
+      </div>
+    </div>
     </div>
   );
 }

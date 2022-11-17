d677deee8626b629702a5c9a4fdc07f40d1d8f57
Jonathan Serdinsky <serdinskyj@gmail.com>
2022-11-17 14:29:56 -0800

    Edited members image + shadow

--- react-frontend/src/components/MembersPage.js
+++ react-frontend/src/components/MembersPage.js
@@ -3,9 +3,9 @@ import * as React from 'react';
 import { Header } from './common/header/Header';
 import './Pages.css';
 import './MembersPage.css';
-import FK from './membersImg/Franz_Kurfess.jpg'
-import NW from './membersImg/Nicholas_Wachter.jpeg'
-import SB from './membersImg/Shaina_Bagri.jpg'
+import FK from './membersImg/Franz_Kurfess.jpg';
+import NW from './membersImg/Nicholas_Wachter.jpeg';
+import SB from './membersImg/Shaina_Bagri.jpg';
 
 export function MembersPage() {
   return (
@@ -15,48 +15,46 @@ export function MembersPage() {
         <h1 className="heading">Members</h1>
       </div>
       <div className="row">
-  
-    <div className="column">
-      <div className="card">
-        <img src={FK} alt ="Franz" width="100" />
-        <div className="container">
-          <h2>Franz Kurfess</h2>
-          <p className="title">Professor Lead</p>
-          <p>Some text that describes Kurfess</p>
-          <p>fkurfess@calpoly.edu</p>
-          {/* <p><button className="button">Contact</button></p> */}
+        <div className="column">
+          <div className="card">
+            <img className="img" src={FK} alt="Franz" width="100" />
+            <div className="container">
+              <h2>Franz Kurfess</h2>
+              <p className="title">Professor Lead</p>
+              <p>Some text that describes Kurfess</p>
+              <p>fkurfess@calpoly.edu</p>
+              {/* <p><button className="button">Contact</button></p> */}
+            </div>
+          </div>
+        </div>
+
+        <div className="column">
+          <div className="card">
+            <img className="img" src={NW} alt="Nick" width="100" />
+            <div className="container">
+              <h2>Nicholas Wachter</h2>
+              <p className="title">Student Research Assistant</p>
+              <p>Some text</p>
+              <p>example@example.com</p>
+              {/* <p><button className="button">Contact</button></p> */}
+            </div>
+          </div>
+        </div>
+
+        <div className="column">
+          <div className="card">
+            <img className="img" src={SB} alt="Shaina" width="100" />
+            <div className="container">
+              <h2>Shaina Bagri</h2>
+              <p className="title">Student Research Assistant</p>
+              <p>Student Research Assistant</p>
+              <p>example@example.com</p>
+              {/* <p><button className="button">Contact</button></p> */}
+            </div>
+          </div>
         </div>
       </div>
     </div>
-
-  <div className="column">
-    <div className="card">
-    <img src={NW} alt ="Nick" width="100" />
-      <div className="container">
-        <h2>Nicholas Wachter</h2>
-        <p className="title">Student Research Assistant</p>
-        <p>Some text</p>
-        <p>example@example.com</p>
-        {/* <p><button className="button">Contact</button></p> */}
-      </div>
-    </div>
-  </div>
-
-  <div className="column">
-    <div className="card">
-    <img src={SB} alt ="Shaina" width="100" />
-      <div className="container">
-        <h2>Shaina Bagri</h2>
-        <p className="title">Student Research Assistant</p>
-        <p>Student Research Assistant</p>
-        <p>example@example.com</p>
-        {/* <p><button className="button">Contact</button></p> */}
-      </div>
-    </div>
-  </div>
-</div>
-    </div>
-    
   );
 }
 

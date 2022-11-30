5547d41a3ce71c54fde082c6610d5f06776ac783
MariRosenwald <45806411+MariRosenwald@users.noreply.github.com>
2022-11-30 12:12:03 -0800

    Update About

--- react-frontend/src/components/AboutPage.js
+++ react-frontend/src/components/AboutPage.js
@@ -3,10 +3,35 @@ import { Header } from './common/header/Header';
 import './Pages.css';
 import GP from './membersImg/GroupPic.jpg'
 
+const image = {
+  width: '100%',
+  height: '300px',
+  
+};
+
+const centered = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  color: 'white'
+};
+
+const container = {
+  position: 'relative',
+  textAlign: 'center'
+};
+
 export function AboutPage() {
   return (
     <div>
       <Header />
+      <div className="bottomHeader">
+        <div style={container}>
+          <img alt="Header" src="/resources/ocean.jpg" style={image} />
+          <h1 style={centered}>Drones for Marine Science and Agriculture</h1>
+        </div>
+      </div>
       <h1 className="heading">About Us</h1>
       <img className='centered' src={GP} alt ="GroupPic" width="400" height="300"/>
       {/* <img className="centered" src="resources/sharkZoom.png" alt="resources/ocean.jpg" /> */}
